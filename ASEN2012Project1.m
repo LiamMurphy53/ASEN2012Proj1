53c28e360b0db7443fc3b93debd2c4bf5a142890
caseygustafson <cagu6020@colorado.edu>
2024-10-29 17:56:08 -0600

    Update ASEN2012Project1.m
    Plots x-y position data and a fit line for each plane, without respect to time

--- ASEN2012Project1.m
+++ ASEN2012Project1.m
@@ -1,1 +1,28 @@
 %% Project 1 ASEN2012
+clc;
+clear;
+close all;
+
+%% Read in  data files
+% in form Time(s), X(nmi), Y(nmi)
+rawdata_A = readmatrix("Data_TCAS_A.csv");
+rawdata_B = readmatrix("Data_TCAS_B.csv");
+
+%% Plot the two on the same graph, without respect to time
+
+figure(1);
+plot(rawdata_A(:,2),rawdata_A(:,3));
+hold on;
+plot(rawdata_B(:,2),rawdata_B(:,3));
+
+% fit line to data A
+[d,S] = polyfit(rawdata_A(:,2),rawdata_A(:,3),1);
+eq_A = @(x) d(1)*x+d(2);
+x = linspace(rawdata_A(1,2),rawdata_A(length(rawdata_A),2),100);
+plot(x,eq_A(x));
+
+%fit line to data B
+[e,T] = polyfit(rawdata_B(:,2),rawdata_B(:,3),1);
+eq_B = @(x) e(1)*x+e(2);
+x = linspace(rawdata_B(1,2),rawdata_B(length(rawdata_A),2),100);
+plot(x,eq_B(x));
